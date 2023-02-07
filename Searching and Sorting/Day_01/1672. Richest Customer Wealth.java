23f6c71679cdbcf52a6cf8cf480de3c0ee92c64c
Om Butala <78205755+OmButala25@users.noreply.github.com>
2023-02-07 12:09:39 +0530

    Add files via upload

---
+++ Searching and Sorting/Day_01/1672. Richest Customer Wealth.java
@@ -0,0 +1,19 @@
+LeetCode - https://leetcode.com/problems/richest-customer-wealth/description/
+
+
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int ans = 0;
+        int sum;
+        for(int i =0; i<accounts.length; i++){
+            sum = 0;
+            for(int j =0; j<accounts[i].length; j++){
+                sum = sum+accounts[i][j];
+            }
+            if(sum > ans){
+                ans = sum;
+            }
+        }
+        return ans;
+    }
+}
